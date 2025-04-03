a4c26f3cfe0f2275d4caf3e2dc6514607c4f00b5
Lara <lajustus48@gmail.com>
2025-04-02 20:24:45 -0600

    Fixed Category Link

--- frontend/src/components/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter.tsx
@@ -14,7 +14,7 @@ function CategoryFilter({
     const fetchCategories = async () => {
       try {
         const response = await fetch(
-          'https://localhost:5000/api/Water/GetProjectTypes'
+          'https://waterproject-justus-backend.azurewebsites.net/api/Water/GetProjectTypes'
         );
         const data = await response.json();
         setCategories(data);
